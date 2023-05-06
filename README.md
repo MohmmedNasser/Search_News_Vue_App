ad288a07378a181def056089d0dddc808d831d72
Mohammed Nasser <31956229+MohmmedNasser@users.noreply.github.com>
2023-05-06 22:47:11 +0300

    upload

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Search_News_Vue_App
+# news-app
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <News :apiKey="apiKey" />
+  </div>
+</template>
+
+<script>
+import News  from "./components/News";
+export default {
+  name: 'app',
+  data: () => {
+    return {
+      apiKey: '82b816f2ea7847a1a2a8a3dd06fb9003'
+    }
+  },
+  components: {
+    News
+  }
+}
+</script>
+
+<style lang="scss">
+ @import url('https://use.fontawesome.com/releases/v5.7.2/css/all.css');
+ body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(to bottom, #80A248, #198B4D);
+  }
+  #app {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    color: #333;
+    width: 100%;
+    max-width: 480px;
+    height: 95%;
+    max-height: 1000px;
+    background-color: #fff;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    
+  }
+  ::-webkit-scrollbar {
+    display: none;
+  }
+</style>
